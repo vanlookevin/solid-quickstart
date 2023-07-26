d2187cf83cc83c16dcee8b0d09b09bec26488865
Kevin Van Loo <74743315+vanlookevin@users.noreply.github.com>
2023-07-26 12:24:48 +0200

    t

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,14 +8,13 @@ export default function Home() {
       <h1>Lex fried chicken!</h1>
       <h1>The best fried chicken in the world!</h1>
       
-            <p>/workspaces/solid-quickstart/Fried-Chicken-Tenders-Recipe-for-Two-11.jpg
-            Fried-Chicken-Tenders-Recipe-for-Two-11.jpg
+            <div>
             <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Fchicken-strips&psig=AOvVaw0thu7Lkpc9s3--uAzWOE19&ust=1690453321449000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCICkmYCTrIADFQAAAAAdAAAAABAE" alt="Ararat mountain" width="256" height="256"></img>
             <img src="Fried-Chicken-Tenders-Recipe-for-Two-11.jpg" alt="Ararat mountain" width="256" height="256"></img>
        
         
 
-      </p>
+      </div>
     </main>
   );
 }
