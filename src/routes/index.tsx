931671d935ed4da483668ddfd0a1ce273d8cbd7b
Kevin Van Loo <74743315+vanlookevin@users.noreply.github.com>
2023-07-26 10:02:56 +0000

    test2

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,8 +8,10 @@ export default function Home() {
       <h1>Lex fried chicken!</h1>
       <h1>The best fried chicken in the world!</h1>
       
-            <p>
-=======
+            <p>/workspaces/solid-quickstart/Fried-Chicken-Tenders-Recipe-for-Two-11.jpg
+            Fried-Chicken-Tenders-Recipe-for-Two-11.jpg
+            <img src="/workspaces/solid-quickstart/Fried-Chicken-Tenders-Recipe-for-Two-11.jpg" alt="Ararat mountain" width="256" height="256"></img>
+            <img src="/Fried-Chicken-Tenders-Recipe-for-Two-11.jpg" alt="Ararat mountain" width="256" height="256"></img>
        
         
 
