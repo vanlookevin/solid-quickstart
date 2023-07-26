b6429a70b5c676f89e7639cac2471d405e3d820d
Kevin Van Loo <74743315+vanlookevin@users.noreply.github.com>
2023-07-26 11:44:52 +0200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,11 +9,8 @@ export default function Home() {
       <h1>The best fried chicken in the world!</h1>
       <Counter />
       <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
+        <img src="Fried-Chicken-Tenders-Recipe-for-Two-11.jpg" alt="Chicken" width="500" height="600">
+        
       </p>
     </main>
   );
